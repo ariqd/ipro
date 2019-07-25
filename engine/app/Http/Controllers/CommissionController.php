7e6a03319e77bf4d45346438863a03b46a8640e7
Alex Hendrawan <alex.hendrawan444@gmail.com>
2019-07-25 13:03:49 +0700

    laporan komisi

--- engine/app/Http/Controllers/CommissionController.php
+++ engine/app/Http/Controllers/CommissionController.php
@@ -33,12 +33,12 @@ class CommissionController extends Controller
         return redirect('finances')->withInfo('Komisi berhasil di set untuk ' . $user->name);
     }
 
-    public function printKomisi($user)
-    {
-        // $pdf = PDF::loadView('print.commission');
-        // return $pdf->download("komisi.pdf");
-        return view("print.commission");
-    }
+    // public function printKomisi($user)
+    // {
+    //     // $pdf = PDF::loadView('print.commission');
+    //     // return $pdf->download("komisi.pdf");
+    //     return view("print.commission");
+    // }
 
     public function show(User $user)
     {
@@ -97,9 +97,69 @@ class CommissionController extends Controller
         return view('finance.commission.show', compact('user', 'sales_orders', 'from', 'to', 'data'));
     }
 
-    // public function count() 
+    // public function count()
     // {
-    //     $user = 
+    //     $user =
     //     //
     // }
+
+
+    public function printKomisi($id)
+    {
+        $user = User::find($id);
+        $settings = Setting::where(['name' => 'finance-period-start'])
+            ->orWhere(['name' => 'finance-period-end'])->get()->keyBy('name');
+
+        // Tanggal periode awal
+        $from = Carbon::create(date('Y'), date('m'), $settings['finance-period-start']->value, 00, 00, 00);
+
+        // Tanggal akhir periode ( + 1 bulan)
+        $to = Carbon::create(date('Y'), date('m') + 1, $settings['finance-period-end']->value, 00, 00, 00);
+
+        $sales_orders = Sale_Detail::join('sales_orders', 'sales_orders.id', '=', 'sales_order_details.sales_order_id')
+            ->where([
+                ['sales_orders.user_id', '=', $user->id],
+                ['sales_orders.no_so', '!=', ''],
+            ])
+            ->whereBetween('sales_orders.created_at', [$from, $to])
+            ->orderBy('sales_orders.created_at', 'desc')
+            ->get();
+
+        // Count commission
+        $sales_orders = $sales_orders->map(function ($value, $key) use ($user) {
+            if ($value->stock->item->category->brand->id == 0) {
+                $value['persen'] = '0,5 %';
+                $value['komisi'] = $value->total * 0.005;
+            } else {
+                $value['persen'] = $user->commission->percentage . ' %';
+                $value['komisi'] = $value->total * ($user->commission->percentage / 100);
+            }
+            $value['buat_sales'] = $value['komisi'] * 0.9;
+            $value['buat_admin'] = $value['komisi'] * 0.1;
+
+            return $value;
+        });
+
+        $data['total'] = 0;
+        $data['total_komisi'] = 0;
+        $data['total_buat_sales'] = 0;
+        $data['total_buat_admin'] = 0;
+        $data['achieved'] = true;
+        $data['percentage'] = 1;
+
+        foreach ($sales_orders as $sales_order) {
+            $data['total'] += $sales_order->total;
+            $data['total_komisi'] += $sales_order->komisi;
+            $data['total_buat_sales'] += $sales_order->buat_sales;
+            $data['total_buat_admin'] += $sales_order->buat_admin;
+        }
+
+        if ($data['total'] < $user->commission->achievement) {
+            $data['achieved'] = false;
+            $data['percentage'] = 0.3;
+        }
+        return view("print.commission", compact('user', 'sales_orders', 'from', 'to', 'data'));
+        // PDF::loadview("print.commission", compact('user', 'sales_orders', 'from', 'to', 'data'));
+
+    }
 }
---
+++ engine/resources/views/print/commission.blade.php
@@ -0,0 +1,346 @@
+@extends("layouts.print")
+
+@section("title")
+Komisi
+@endsection
+
+@push("css")
+
+@endpush
+
+@section("content")
+<div class="container-fluid">
+    <div class="col-xs-12">
+        <div class="col-xs-6">
+            <img src="{!! asset('assets/img/logo.png') !!}" alt="" width="150">
+        </div>
+
+        <div class="col-xs-6">
+            <h5>PT INDOTEKNIK PRATAMA PRO</h5>
+            <h6 style="font-size:10px; line-height: 0.2px">Jl. Jendral Sudirman No. 672 B</h6>
+            <h6 style="font-size:10px; line-height: 0.2px">Bandung, Jawa Barat</h6>
+            <h6 style="font-size:10px; line-height: 0.2px">Telp. 022 2057 33 22</h6>
+            <h6 style="font-size:10px; line-height: 0.2px">Mobile 0819 0123 3030</h6>
+        </div>
+    </div>
+    <div class="col-xs-12" style="text-align:center; padding: 2% 0 2% 0">
+        FORM KOMISI SALES
+    </div>
+    <div class="col-xs-12">
+
+
+        <div class="row">
+            <div class="col-6">
+                <h4>Periode {{ $from->format('d F') }} - {{ $to->format('d F') }}</h4>
+            </div>
+            <div class="col-6">
+                <h4 class="float-right">
+                    Achievement: Rp {{ number_format($user->commission->achievement, '0', ',', '.') }}
+                </h4>
+            </div>
+            <div class="col-12">
+                <table class="table table-light table-bordered table-hover">
+                    <thead class="text-center">
+                        <tr>
+                            <th rowspan="2" class="align-middle">No</th>
+                            <th rowspan="2" class="align-middle">Keterangan</th>
+                            <th>Total (Exclude PPN)</th>
+                            <th>Komisi</th>
+                            <th>(-10 %)</th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <span class="float-right font-weight-bold">
+                                    @if ($data['achieved'])
+                                    <span class="badge badge-success">Achieve</span>
+                                    @else
+                                    <span class="badge badge-danger">Tidak Achieve</span>
+                                    @endif
+                                    Rp {{ number_format($data['total']) }}
+                                </span>
+                            </th>
+                            <th>
+                                <span class="float-right font-weight-bold">
+                                    Rp {{ number_format($data['total_komisi']) }}
+                                </span>
+                            </th>
+                            <th>
+                                <span class="float-right font-weight-bold">
+                                    Rp {{ number_format($data['total_buat_sales']) }}
+                                </span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($sales_orders as $sales_order)
+                        <tr>
+                            <td class="align-middle">{{ $loop->iteration }}</td>
+                            <td class="align-middle">
+                                <small class="text-secondary">
+                                    {{ $sales_order->stock->item->category->brand->name }}
+                                    -
+                                    {{ $sales_order->stock->item->category->name }}
+                                </small> <br>
+                                {{ $sales_order->stock->item->name }} ({{ $sales_order->persen }})
+                            </td>
+                            <td class="align-middle">
+                                <span class="float-right">Rp {{ number_format($sales_order->total) }}</span>
+                            </td>
+                            <td class="align-middle">
+                                <span class="float-right">Rp {{ number_format($sales_order->komisi) }}</span>
+                            </td>
+                            <td class="align-middle">
+                                <span class="float-right">Rp {{ number_format($sales_order->buat_sales) }}</span>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colspan="2">
+                                <span class="float-right font-weight-bold">
+                                    Total:
+                                </span>
+                            </td>
+                            <td>
+                                <span class="float-right font-weight-bold">
+                                    Rp {{ number_format($data['total']) }}
+                                </span>
+                            </td>
+                            <td>
+                                <span class="float-right font-weight-bold">
+                                    Rp {{ number_format($data['total_komisi']) }}
+                                </span>
+                            </td>
+                            <td>
+                                <span class="float-right font-weight-bold">
+                                    Rp {{ number_format($data['total_buat_sales']) }}
+                                </span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                <span class="float-right font-weight-bold">
+                                    Status:
+                                </span>
+                            </td>
+                            <td>
+                                <h4 class="float-right font-weight-bold">
+                                    @if ($data['achieved'])
+                                    <span class="badge badge-success">Achieve</span>
+                                    @else
+                                    <span class="badge badge-danger">Tidak Achieve</span>
+                                    @endif
+                                </h4>
+                            </td>
+                            <td>
+                                <span class="float-right font-weight-bold">
+                                    Rp {{ number_format($data['total_komisi'] * $data['percentage']) }}
+                                </span>
+                            </td>
+                            <td>
+                                <span class="float-right font-weight-bold">
+                                    Rp {{ number_format($data['total_buat_sales'] * $data['percentage']) }}
+                                </span>
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xs-12">
+        <div class="col-xs-6"></div>
+        <div class="col-xs-6" style="text-align: right">Bandung, {{ date(" d F Y") }}</div>
+    </div>
+    <style>
+        td {
+            text-align: center;
+        }
+    </style>
+    <div class="col-xs-12" style="padding-top: 5%">
+        <table class="table">
+            <tr>
+                <td>(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)
+                </td>
+                <td>IRENE</td>
+            </tr>
+
+            <tr>
+                <td>Sales</td>
+                <td>(Finance)</td>
+            </tr>
+        </table>
+    </div>
+</div>
+</div>
+
+
+<div class="container-fluid" style="padding-top: 10%">
+    <div class="col-xs-12">
+        <div class="col-xs-6">
+            <img src="{!! asset('assets/img/logo.png') !!}" alt="" width="150">
+        </div>
+
+        <div class="col-xs-6">
+            <h5>PT INDOTEKNIK PRATAMA PRO</h5>
+            <h6 style="font-size:10px; line-height: 0.2px">Jl. Jendral Sudirman No. 672 B</h6>
+            <h6 style="font-size:10px; line-height: 0.2px">Bandung, Jawa Barat</h6>
+            <h6 style="font-size:10px; line-height: 0.2px">Telp. 022 2057 33 22</h6>
+            <h6 style="font-size:10px; line-height: 0.2px">Mobile 0819 0123 3030</h6>
+        </div>
+    </div>
+    <div class="col-xs-12" style="text-align:center; padding: 2% 0 2% 0">
+        FORM KOMISI SALES
+    </div>
+    <div class="col-xs-12">
+
+
+        <div class="row">
+            <div class="col-6">
+                <h4>Periode {{ $from->format('d F') }} - {{ $to->format('d F') }}</h4>
+            </div>
+            <div class="col-6">
+                <h4 class="float-right">
+                    Achievement: Rp {{ number_format($user->commission->achievement, '0', ',', '.') }}
+                </h4>
+            </div>
+            <div class="col-12">
+                <table class="table table-light table-bordered table-hover">
+                    <thead class="text-center">
+                        <tr>
+                            <th rowspan="2" class="align-middle">No</th>
+                            <th rowspan="2" class="align-middle">Keterangan</th>
+                            <th>Total (Exclude PPN)</th>
+                            <th>Komisi</th>
+                            <th>(-10 %)</th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <span class="float-right font-weight-bold">
+                                    @if ($data['achieved'])
+                                    <span class="badge badge-success">Achieve</span>
+                                    @else
+                                    <span class="badge badge-danger">Tidak Achieve</span>
+                                    @endif
+                                    Rp {{ number_format($data['total']) }}
+                                </span>
+                            </th>
+                            <th>
+                                <span class="float-right font-weight-bold">
+                                    Rp {{ number_format($data['total_komisi']) }}
+                                </span>
+                            </th>
+                            <th>
+                                <span class="float-right font-weight-bold">
+                                    Rp {{ number_format($data['total_buat_sales']) }}
+                                </span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($sales_orders as $sales_order)
+                        <tr>
+                            <td class="align-middle">{{ $loop->iteration }}</td>
+                            <td class="align-middle">
+                                <small class="text-secondary">
+                                    {{ $sales_order->stock->item->category->brand->name }}
+                                    -
+                                    {{ $sales_order->stock->item->category->name }}
+                                </small> <br>
+                                {{ $sales_order->stock->item->name }} ({{ $sales_order->persen }})
+                            </td>
+                            <td class="align-middle">
+                                <span class="float-right">Rp {{ number_format($sales_order->total) }}</span>
+                            </td>
+                            <td class="align-middle">
+                                <span class="float-right">Rp {{ number_format($sales_order->komisi) }}</span>
+                            </td>
+                            <td class="align-middle">
+                                <span class="float-right">Rp {{ number_format($sales_order->buat_sales) }}</span>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colspan="2">
+                                <span class="float-right font-weight-bold">
+                                    Total:
+                                </span>
+                            </td>
+                            <td>
+                                <span class="float-right font-weight-bold">
+                                    Rp {{ number_format($data['total']) }}
+                                </span>
+                            </td>
+                            <td>
+                                <span class="float-right font-weight-bold">
+                                    Rp {{ number_format($data['total_komisi']) }}
+                                </span>
+                            </td>
+                            <td>
+                                <span class="float-right font-weight-bold">
+                                    Rp {{ number_format($data['total_buat_sales']) }}
+                                </span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                <span class="float-right font-weight-bold">
+                                    Status:
+                                </span>
+                            </td>
+                            <td>
+                                <h4 class="float-right font-weight-bold">
+                                    @if ($data['achieved'])
+                                    <span class="badge badge-success">Achieve</span>
+                                    @else
+                                    <span class="badge badge-danger">Tidak Achieve</span>
+                                    @endif
+                                </h4>
+                            </td>
+                            <td>
+                                <span class="float-right font-weight-bold">
+                                    Rp {{ number_format($data['total_komisi'] * $data['percentage']) }}
+                                </span>
+                            </td>
+                            <td>
+                                <span class="float-right font-weight-bold">
+                                    Rp {{ number_format($data['total_buat_sales'] * $data['percentage']) }}
+                                </span>
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+
+        <div class="col-xs-12">
+            <div class="col-xs-6"></div>
+            <div class="col-xs-6" style="text-align: right">Bandung, {{ date(" d F Y") }}</div>
+        </div>
+        <style>
+            td {
+                text-align: center;
+            }
+        </style>
+        <div class="col-xs-12" style="padding-top: 5%">
+            <table class="table">
+                <tr>
+                    <td>(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)
+                    </td>
+                    <td>IRENE</td>
+                </tr>
+
+                <tr>
+                    <td>Sales</td>
+                    <td>(Finance)</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+
+@endsection
