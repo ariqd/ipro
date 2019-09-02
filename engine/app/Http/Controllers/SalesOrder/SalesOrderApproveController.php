e61b00bda9f432b0e7800bd75f1e016e7317f729
Ariq Daffa <ariqdaffaathallah@gmail.com>
2019-09-02 21:40:23 +0700

    PO nambahin total berat sama number_format

--- engine/app/Http/Controllers/SalesOrder/SalesOrderApproveController.php
+++ engine/app/Http/Controllers/SalesOrder/SalesOrderApproveController.php
@@ -50,9 +50,8 @@ class SalesOrderApproveController extends Controller
 
         //calculatecommission
         $sales_orders = Sale_Detail::join('sales_orders', 'sales_orders.id', '=', 'sales_order_details.sales_order_id')
-        ->where("sales_orders.id","=",$id)
-        ->get();
-
+            ->where("sales_orders.id", "=", $id)
+            ->get();
 
         $totalachieve = 0;
         $totalachievesales = 0;
@@ -60,7 +59,7 @@ class SalesOrderApproveController extends Controller
 
         $totalSO = 0;
 
-        $totalkomisiachieve =0;
+        $totalkomisiachieve = 0;
         $totalkomisinotachieve = 0;
 
         $totalkomisiachievesales = 0;
@@ -80,8 +79,8 @@ class SalesOrderApproveController extends Controller
                         $value['komisi_achieve'] = $value->total * 0.02 * 0.01;
                         $value['komisi_achieve_referal'] = $value->total * 0.02 * 0.01;
                     }
-                    $totalachieve += $value->total* 0.5;
-                    $totalachievesales += $value->total* 0.5;
+                    $totalachieve += $value->total * 0.5;
+                    $totalachievesales += $value->total * 0.5;
                     $role = "Referral";
                 } elseif ($value->admin_id != null) {
                     if ($value->stock->item->category->brand->id == 0) {
@@ -91,10 +90,9 @@ class SalesOrderApproveController extends Controller
                         $value['komisi_achieve'] = $value->total * 0.02 * 0.01 * 0.016;
                         $value['komisi_achieve_admin'] = $value->total * 0.02 * 0.01 * 0.04;
                     }
-                    $totalachieve += $value->total* 0.8;
-                    $totalachieveadmin += $value->total* 0.2;
+                    $totalachieve += $value->total * 0.8;
+                    $totalachieveadmin += $value->total * 0.2;
                     $role = "Admin";
-
                 } else {
                     if ($value->stock->item->category->brand->id == 0) {
                         $value['komisi_achieve'] = $value->total * 0.005 * 0.02;
@@ -115,7 +113,6 @@ class SalesOrderApproveController extends Controller
                     $totalachieve += $value->total;
                     $totalachievesales += $value->total;
                     $role = "Sales";
-
                 } elseif ($value->admin_id != null) {
                     if ($value->stock->item->category->brand->id == 0) {
                         $value['komisi_achieve'] = $value->total * 0.005 * 0.015;
@@ -124,10 +121,9 @@ class SalesOrderApproveController extends Controller
                         $value['komisi_achieve'] = $value->total * 0.02 * 0.015;
                         $value['komisi_achieve_admin'] = $value->total * 0.02 * 0.005;
                     }
-                    $totalachieve += $value->total* 0.8;
-                    $totalachieveadmin += $value->total* 0.2;
+                    $totalachieve += $value->total * 0.8;
+                    $totalachieveadmin += $value->total * 0.2;
                     $role = "Admin";
-
                 } else {
                     if ($value->stock->item->category->brand->id == 0) {
                         $value['komisi_achieve'] = $value->total * 0.005 * 0.02;
@@ -148,7 +144,6 @@ class SalesOrderApproveController extends Controller
             $totalkomisinotachieveadmin += $value['komisi_achieve_admin'] * 0.3;
 
             $totalSO += $value->total;
-
         }
 
         //endcalulatecommission
@@ -161,15 +156,15 @@ class SalesOrderApproveController extends Controller
         $commission->save();
 
         Commission_Detail::create([
-            "user_id"=>$sale->user->id,
-            "commission"=> $totalkomisiachieve,
-            "commission_not_achieve"=> $totalkomisinotachieve,
-            "sales_order_id"=>$sale->id,
-            "role"=>$role,
+            "user_id" => $sale->user->id,
+            "commission" => $totalkomisiachieve,
+            "commission_not_achieve" => $totalkomisinotachieve,
+            "sales_order_id" => $sale->id,
+            "role" => $role,
         ]);
 
         //save komisi jika bahu membahu
-        if($sale->sales_id != null){
+        if ($sale->sales_id != null) {
             $commission = Commission::where('user_id', $sale->sales_id)->first();
             $commission->total_commission += $totalkomisiachievesales;
             $commission->total_commission_not_achieve += $totalkomisinotachievesales;
@@ -177,29 +172,29 @@ class SalesOrderApproveController extends Controller
             $commission->save();
 
             Commission_Detail::create([
-                "user_id"=>$sale->sales_id,
-                "commission"=> $totalkomisiachievesales,
-                "commission_not_achieve"=> $totalkomisinotachievesales,
-                "sales_order_id"=>$sale->id,
-                "role"=>$role,
+                "user_id" => $sale->sales_id,
+                "commission" => $totalkomisiachievesales,
+                "commission_not_achieve" => $totalkomisinotachievesales,
+                "sales_order_id" => $sale->id,
+                "role" => $role,
             ]);
         }
 
         //save komisi jika ada admin
-        if($sale->admin_id != null){
+        if ($sale->admin_id != null) {
             $commission = Commission::where('user_id', $sale->admin_id)->first();
             $commission->total_commission += $totalkomisiachieveadmin;
             $commission->total_commission_not_achieve += $totalkomisinotachieveadmin;
             $commission->achieved += $totalachieveadmin;
             $commission->save();
             Commission_Detail::create([
-                "user_id"=>$sale->admin_id,
-                "commission"=> $totalkomisiachieveadmin,
-                "commission_not_achieve"=> $totalkomisinotachieveadmin,
-                "sales_order_id"=>$sale->id,
-                "role"=>$role,
+                "user_id" => $sale->admin_id,
+                "commission" => $totalkomisiachieveadmin,
+                "commission_not_achieve" => $totalkomisinotachieveadmin,
+                "sales_order_id" => $sale->id,
+                "role" => $role,
             ]);
-            }
+        }
         return redirect()->back()->with("info", "SO berhasil disetujui, <a href='approve/print' class='btn btn-light'>Print Kwitansi</a>");
     }
 
@@ -216,6 +211,6 @@ class SalesOrderApproveController extends Controller
         $data["SO"] = $sale->no_so;
         dd($data);
         $pdf = PDF::loadView('print.kwitansi', $data);
-        return $pdf->stream('invoice '.date("Ymd").'.pdf');
+        return $pdf->stream('invoice ' . date("Ymd") . '.pdf');
     }
 }
--- engine/resources/views/purchase/show.blade.php
+++ engine/resources/views/purchase/show.blade.php
@@ -3,204 +3,221 @@
 @section('title', 'Purchase Order')
 
 @push("css")
-    <style>
-        .loading {
-            background: lightgrey;
-            padding: 15px;
-            position: fixed;
-            border-radius: 4px;
-            left: 50%;
-            top: 50%;
-            text-align: center;
-            margin: -40px 0 0 -50px;
-            z-index: 2000;
-            display: none;
-        }
+<style>
+    .loading {
+        background: lightgrey;
+        padding: 15px;
+        position: fixed;
+        border-radius: 4px;
+        left: 50%;
+        top: 50%;
+        text-align: center;
+        margin: -40px 0 0 -50px;
+        z-index: 2000;
+        display: none;
+    }
 
-        /* The switch - the box around the slider */
-        .switch {
-            position: relative;
-            display: inline-block;
-            width: 60px;
-            height: 34px;
-        }
+    /* The switch - the box around the slider */
+    .switch {
+        position: relative;
+        display: inline-block;
+        width: 60px;
+        height: 34px;
+    }
 
-        /* Hide default HTML checkbox */
-        .switch input {
-            opacity: 0;
-            width: 0;
-            height: 0;
-        }
+    /* Hide default HTML checkbox */
+    .switch input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+    }
 
-        /* The slider */
-        .slider {
-            position: absolute;
-            cursor: pointer;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background-color: #ccc;
-            -webkit-transition: .4s;
-            transition: .4s;
-        }
+    /* The slider */
+    .slider {
+        position: absolute;
+        cursor: pointer;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #ccc;
+        -webkit-transition: .4s;
+        transition: .4s;
+    }
 
-        .slider:before {
-            position: absolute;
-            content: "";
-            height: 26px;
-            width: 26px;
-            left: 4px;
-            bottom: 4px;
-            background-color: white;
-            -webkit-transition: .4s;
-            transition: .4s;
-        }
+    .slider:before {
+        position: absolute;
+        content: "";
+        height: 26px;
+        width: 26px;
+        left: 4px;
+        bottom: 4px;
+        background-color: white;
+        -webkit-transition: .4s;
+        transition: .4s;
+    }
 
-        input:checked + .slider {
-            background-color: #2196F3;
-        }
+    input:checked+.slider {
+        background-color: #2196F3;
+    }
 
-        input:focus + .slider {
-            box-shadow: 0 0 1px #2196F3;
-        }
+    input:focus+.slider {
+        box-shadow: 0 0 1px #2196F3;
+    }
 
-        input:checked + .slider:before {
-            -webkit-transform: translateX(26px);
-            -ms-transform: translateX(26px);
-            transform: translateX(26px);
-        }
+    input:checked+.slider:before {
+        -webkit-transform: translateX(26px);
+        -ms-transform: translateX(26px);
+        transform: translateX(26px);
+    }
 
-        /* Rounded sliders */
-        .slider.round {
-            border-radius: 34px;
-        }
+    /* Rounded sliders */
+    .slider.round {
+        border-radius: 34px;
+    }
 
-        .slider.round:before {
-            border-radius: 50%;
-        }
-    </style>
+    .slider.round:before {
+        border-radius: 50%;
+    }
+
+</style>
 @endpush
 
 @push("js")
 @endpush
 
 @section('content')
-    {{--@include('layouts.ajax', ['size' => 'lg'])--}}
-    <div class="loading">
-        <i class="fas fa-sync fa-spin fa-2x fa-fw"></i><br/>
-        <span>Loading</span>
+{{--@include('layouts.ajax', ['size' => 'lg'])--}}
+<div class="loading">
+    <i class="fas fa-sync fa-spin fa-2x fa-fw"></i><br />
+    <span>Loading</span>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            @include("layouts.feedback")
+            <div class="d-flex justify-content-between">
+                <div>
+                    <h2>
+                        <small>
+                            <a href="{{ url('purchase-orders') }}" class="text-dark">Purchase Orders</a> /
+                        </small>
+                        <b>Detail</b>
+                    </h2>
+                </div>
+                <div>
+                    <a href="{{ url('purchase-orders') }}" class="btn btn-light"><i class="fa fa-arrow-left"></i> Purchase Orders</a>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="container">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="form-group row">
+                <label for="payment_method" class="col-4 col-form-label">Purchase Order ID</label>
+                <div class="col-7">
+                    <input disabled="" type="text" class="form-control" id="customer" name="po_order"
+                        value="{{ $header->purchase_number }}">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 d-none">
+            <div class="form-group row">
+                <label for="payment_method" class="col-4 col-form-label">Sales Order ID</label>
+                <div class="col-7">
+                    <input type="text" autocomplete="off" onchange="searchSales()" class="form-control"
+                        id="salesorderid" name="so_order">
+                </div>
+            </div>
+        </div>
+    </div>
+    <form action="{{ url("purchase-orders/$header->id/approve") }}" method="post">
         <div class="row">
             <div class="col-lg-12">
-                @include("layouts.feedback")
-                <div class="d-flex justify-content-between">
-                    <div>
-                        <h2>
-                            {{--Buat Purchase Order Baru--}}
-                            <small>
-                                <a href="{{ url('purchase-orders') }}" class="text-dark">Purchase Orders</a> /
-                            </small>
-                            <b>Show</b>
-                        </h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="form-group row">
-                    <label for="payment_method" class="col-4 col-form-label">Purchase Order ID</label>
-                    <div class="col-7">
-                        <input disabled="" type="text" class="form-control" id="customer" name="po_order"
-                               value="{{ $header->purchase_number }}">
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6 d-none">
-                <div class="form-group row">
-                    <label for="payment_method" class="col-4 col-form-label">Sales Order ID</label>
-                    <div class="col-7">
-                        <input type="text" autocomplete="off" onchange="searchSales()" class="form-control"
-                               id="salesorderid" name="so_order">
-                    </div>
-                </div>
-            </div>
-        </div>
-        <form action="{{ url("purchase-orders/$header->id/approve") }}" method="post">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h4>List PO</h4>
-                    <div class="table-responsive">
-                        <table class="table table-bordered table-light">
-                            <thead>
+                <h4>List PO</h4>
+                <div class="table-responsive">
+                    <table class="table table-hover table-light border">
+                        <thead>
                             <tr>
                                 <th>No.</th>
-                                <th>Kategori</th>
-                                <th>Kode Barang</th>
-                                <th>Item</th>
+                                <th>Produk</th>
                                 <th>Berat/pcs</th>
                                 <th>Order Qty/pcs</th>
-                                <th>Approval Qty/pcs</th>
-                                <th>Price/pcs</th>
+                                <th>Approved Qty/pcs</th>
                                 <th>Total Amount (IDR)</th>
-                                @if(Gate::allows('isFinance') || Gate::allows('isAdmin'))
-                                    <th>Approve</th>
+                                @if(Gate::allows('isFinance') || Gate::allows('isAdmin') && $create)
+                                <th>Approve</th>
                                 @endif
                             </tr>
-                            </thead>
-                            <tbody id="purchase-body">
+                        </thead>
+                        <tbody id="purchase-body">
                             @csrf
                             @foreach($line as $key)
-                                <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $key->item->category->brand->name }} {{ $key->item->category->name }}</td>
-                                    <td>{{ $key->item->code }}</td>
-                                    <td>{{ $key->item->name }}</td>
-                                    <td>{{ $key->item->weight }}</td>
-                                    <td>{{ $key->qty }}</td>
-                                    @if(Gate::allows('isFinance') || Gate::allows('isAdmin'))
-                                        @if($key->approval_finance > 1)
-                                            <td><input type="number" min="1" max="{{ $key->qty }}"
-                                                       value="{{ $key->qty }}" name="{{ "qty-".$key->id }}"
-                                                       class="form-control"></td>
-                                        @else
-                                            <td>{{ $key->qty_approval }}</td>
-                                        @endif
-                                    @else
-                                        <td>{{ $key->qty_approval }}</td>
-                                    @endif
-                                    <td>{{ $key->purchase_price }}</td>
-                                    <td>{{ $key->total_price }}</td>
-                                    @if(Gate::allows('isFinance') || Gate::allows('isAdmin'))
-                                        <td><label class="switch">
-                                                <input @if($key->approval_finance == 0)  @else checked=""
-                                                       @endif type="checkbox" name="{{"approve-".$key->id }}">
-                                                <span class="slider"></span>
-                                            </label>
-                                        </td>
-                                    @endif
-                                </tr>
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>
+                                    <small class="text-secondary">{{ $key->item->category->brand->name }} -
+                                        {{ $key->item->category->name }}</small> <br>
+                                    {{ $key->item->code }} - {{ $key->item->name }}
+                                </td>
+                                <td>{{ $key->item->weight }} Kg</td>
+                                <td>{{ $key->qty }} pcs</td>
+                                @if(Gate::allows('isFinance') || Gate::allows('isAdmin'))
+                                @if($key->approval_finance > 1)
+                                <td><input type="number" min="1" max="{{ $key->qty }}" value="{{ $key->qty }}"
+                                        name="{{ "qty-".$key->id }}" class="form-control"></td>
+                                @else
+                                <td>{{ $key->qty_approval }} pcs</td>
+                                @endif
+                                @else
+                                <td>{{ $key->qty_approval }} pcs</td>
+                                @endif
+                                <td class="text-right">Rp{{ number_format($key->total_price, 0, ',', '.') }}</td>
+                                @if(Gate::allows('isFinance') || Gate::allows('isAdmin') && $create)
+                                <td>
+                                    <label class="switch">
+                                        <input @if($key->approval_finance == 0) @else checked=""
+                                        @endif type="checkbox" name="{{"approve-".$key->id }}">
+                                        <span class="slider"></span>
+                                    </label>
+                                </td>
+                                @endif
+                            </tr>
                             @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="2"></td>
+                                <td>
+                                    {{ $total_weight }} Kg
+                                </td>
+                                <td>
+                                    {{ $total_qty }} pcs
+                                </td>
+                                <td>
+                                    {{ $total_qty_approved }} pcs
+                                </td>
+                                <td class="text-right">
+                                    Rp{{ number_format($total_amount, 0, ',', '.') }}
+                                </td>
+                            </tr>
+                        </tfoot>
+                    </table>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-lg-6">
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
 
-                </div>
-                <div class="col-lg-6">
-                    @if(Gate::allows('isFinance') || Gate::allows('isAdmin'))
-                        @if($header->approval_status == 0)
-                            <input type="submit" class="form-control btn btn-success" value="Create Purchase Order">
-                        @endif
-                    @endif
-                </div>
             </div>
-        </form>
-    </div>
+            <div class="col-lg-6">
+                @if(Gate::allows('isFinance') || Gate::allows('isAdmin'))
+                @if($header->approval_status == 0)
+                <input type="submit" class="form-control btn btn-success" value="Create Purchase Order">
+                @endif
+                @endif
+            </div>
+        </div>
+    </form>
+</div>
 @endsection
