ecf037d3bbfe7e4c3d9504e7753faa66ff8b99e8
Ariq Daffa <ariqdaffaathallah@gmail.com>
2019-09-15 13:19:02 +0700

    teuing naon

--- engine/config/snappy.php
+++ engine/config/snappy.php
@@ -5,14 +5,14 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  =>'"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"',
+        'binary'  =>'"D:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary'  => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage"',
+        'binary'  => '"D:\Program Files\wkhtmltopdf\bin\wkhtmltoimage"',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
